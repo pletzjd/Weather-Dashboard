ca319c588ff6a93170d0ef77eb9260f511a6aff8
pletzjd <j.pletzer17@gmail.com>
2022-05-31 22:03:09 -0400

    Retrieve lat and lon of the city. Retrive weather data from api. Delayed weather data retrival to give lat and lon retrieval time to complete first."

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,22 +1,52 @@
 let searchButton = $('#searchButton');
 let searchBar = $('#searchBar');
-let city = [];
+let cities = [];
 let searchHistory = $('#searchHistory');
+let APIKey = '84d085c88283aedb8eea3df65fad1fc6';
+let currentCity = '';
+let longitude = '';
+let latitude = '';
 
+function getLatLon(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+currentCity+'&appid='+APIKey)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        latitude = data.coord.lat;
+        longitude = data.coord.lon;
+    });
+}
+function getWeather(){
+
+    fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+String(latitude)+'&lon='+String(longitude)+'&exclude=minutely,hourly&appid='+APIKey)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+}
 
 function searchFunc(event){
     event.stopPropagation();
     event.preventDefault();
     
+    currentCity = searchBar.val();
     
-    city.push(searchBar.val());
+    cities.push(currentCity);
+    localStorage.setItem('cities',cities);
 
-    for (i=0;i<city.length;i++){
-        let history = $('<p>');
-        history.text(city[i]);
-        searchHistory.append('<li><button>'+history.text()+'</button></li>');
-    }
+    let history = $('<button>');
+    history.val(currentCity);
+    searchHistory.append('<li><button>'+history.val()+'</button></li>');
 
+    getLatLon();
+    
+    setTimeout(getWeather,1000)
+    
+    
+    
 }
 
 searchButton.on('click', searchFunc)
